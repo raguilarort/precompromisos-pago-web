64dc511f67101619c29e1e054a3facdf04067a82
RICARDO AGUILAR ORTEGA <ricardo.aguilar@senado.gob.mx>
2026-07-08 14:20:09 -0600

    feat. creación de archivos para los ambientes

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:5001/api', 
+  msalConfig: {
+    clientId: '79d4f98f-283b-43bd-aeca-303cc6a3f8d8',
+    authority: 'https://login.microsoftonline.com/ec98a3f2-ab05-4f1a-a1a7-9f2cba375ebb',
+    redirectUri: 'http://localhost:4200',
+  },
+};
