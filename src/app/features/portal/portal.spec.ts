872fee3a1f678f09a405ca5d9b044530958d9c6a
Ricardo Aguilar <ricardo.aguilarort@outlook.com>
2026-07-08 14:24:23 -0600

    feat. creación básica de features de la aplicación

---
+++ src/app/features/portal/portal.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Portal } from './portal';
+
+describe('Portal', () => {
+  let component: Portal;
+  let fixture: ComponentFixture<Portal>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Portal],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Portal);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/portal/portal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portal',
+  imports: [],
+  templateUrl: './portal.html',
+  styleUrl: './portal.css',
+})
+export class Portal {}
