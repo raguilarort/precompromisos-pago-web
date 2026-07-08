c57061f39393349b33d416ebaafe2f78f2c09d91
Ricardo Aguilar <ricardo.aguilarort@outlook.com>
2026-07-08 15:10:18 -0600

    feat. creación del navbar e integración con el home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from '../../shared/layout/navbar/navbar';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
