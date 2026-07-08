f7d2099364d175f06b6d5193253218718fce11d8
Ricardo Aguilar <ricardo.aguilarort@outlook.com>
2026-07-08 15:03:37 -0600

    feat. se agrega pagina inicial

--- src/app/features/portal/portal.ts
+++ src/app/features/portal/portal.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-portal',
@@ -6,4 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './portal.html',
   styleUrl: './portal.css',
 })
-export class Portal {}
+export class Portal {
+  // Inyectamos el Router de Angular usando la sintaxis moderna
+  private router = inject(Router);
+
+  iniciarSesion() {
+    // Aquí más adelante irá la lógica real de MSAL.
+    // Por ahora, simulamos un login exitoso redirigiendo al home.
+    console.log('Simulando redirección a Microsoft Entra ID...');
+    this.router.navigate(['/home']);
+  }
+}
