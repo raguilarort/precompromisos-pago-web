b789afe6378e08425f2fb3d6b08dbb6b00517c23
Ricardo Aguilar <ricardo.aguilarort@outlook.com>
2026-07-22 11:49:05 -0600

    feat. integración del selector de ejercicio en el navbar de la aplicación

--- src/app/shared/layout/navbar/navbar.ts
+++ src/app/shared/layout/navbar/navbar.ts
@@ -1,10 +1,12 @@
 import { Component, inject, computed } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router'; // Nuevas importaciones
 import { Auth } from '../../../core/auth/auth';
+// Importamos el nuevo componente aislado para la seleccion de ejercicios
+import { SelectorEjercicio } from '../../components/selector-ejercicio/selector-ejercicio';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, SelectorEjercicio],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
