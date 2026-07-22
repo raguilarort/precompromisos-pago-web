d8089ca3cbc42b3fc459d90ed7b77f0a41a98f98
Ricardo Aguilar <ricardo.aguilarort@outlook.com>
2026-07-22 11:39:06 -0600

    feat. primera versión de la interfaz para ver el detalle de un precompromiso individualmente

--- src/app/features/precompromisos/detail/detail.ts
+++ src/app/features/precompromisos/detail/detail.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
+import { PrecompromisoService } from '../services/precompromisos/precompromisos';
+import { Precompromiso } from '../models/precompromiso.model';
 
 @Component({
   selector: 'app-detail',
-  imports: [],
+  imports: [RouterLink, CurrencyPipe],
   templateUrl: './detail.html',
   styleUrl: './detail.css',
 })
-export class Detail {}
+export class Detail implements OnInit {
+  private route = inject(ActivatedRoute);
+  private precompromisoService = inject(PrecompromisoService);
+
+  // Signal para almacenar los datos del precompromiso
+  registro = signal<Precompromiso | undefined>(undefined);
+
+  ngOnInit() {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    if (idParam) {
+      // Convertimos el ID de la ruta (string) a número para buscarlo
+      const data = this.precompromisoService.obtenerPorId(Number(idParam));
+      this.registro.set(data);
+    }
+  }
+}
