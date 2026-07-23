7f0989f48e0cee71e4d03c38bd70bb44ed8ff4c9
Ricardo Aguilar <ricardo.aguilarort@outlook.com>
2026-07-23 13:47:57 -0600

    feat. cambio en la forma de mostra el nombre del rol de usuario autenticado dentro del navbar

--- src/app/shared/layout/navbar/navbar.ts
+++ src/app/shared/layout/navbar/navbar.ts
@@ -1,6 +1,7 @@
 import { Component, inject, computed } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router'; // Nuevas importaciones
 import { Auth } from '../../../core/auth/auth';
+import { RolSistema } from '../../../core/auth/models/auth.model'
 // Importamos el nuevo componente aislado para la seleccion de ejercicios
 import { SelectorEjercicio } from '../../components/selector-ejercicio/selector-ejercicio';
 
@@ -12,4 +13,18 @@ import { SelectorEjercicio } from '../../components/selector-ejercicio/selector-
 })
 export class Navbar {
   authService = inject(Auth);
+
+  // 1. Exponemos el Enum completo a la vista HTML
+  RolSistema = RolSistema;
+  
+  // 2. Extraemos dinámicamente solo los IDs numéricos [1, 2, 3, 4, 5]
+  // Si mañana agregas un nuevo rol al Enum, aparecerá aquí automáticamente.
+  rolesIds = Object.values(RolSistema).filter(v => typeof v === 'number') as number[];
+
+  // 3. Señal computada para renderizar dinámicamente el perfil
+  // Usa el Mapeo Inverso: RolSistema[1] devuelve "Consultor"
+  nombreRolActual = computed(() => {
+    const usuario = this.authService.usuarioAutenticado();
+    return usuario ? RolSistema[usuario.rol] : 'Cargando...';
+  });
 }
