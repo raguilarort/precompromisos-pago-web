2bb19c1273fec9e5334af1e3762f123dfece226a
Ricardo Aguilar <ricardo.aguilarort@outlook.com>
2026-08-07 15:01:29 -0600

    feat. actualización de la URL de la API en el entorno de desarrollo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:5001/api', 
+  apiUrl: 'http://localhost:8080/api/v1', 
   msalConfig: {
     clientId: '79d4f98f-283b-43bd-aeca-303cc6a3f8d8',
     authority: 'https://login.microsoftonline.com/ec98a3f2-ab05-4f1a-a1a7-9f2cba375ebb',
