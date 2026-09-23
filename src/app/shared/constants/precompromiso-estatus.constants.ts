5196ff3b81e14e0b353b4f93c286bd8831e49974
Ricardo Aguilar <ricardo.aguilarort@outlook.com>
2026-09-23 14:53:46 -0600

    feat. se agrega constante para el catálogo de estatus del precompromiso

--- src/app/shared/constants/precompromiso-estatus.constants.ts
+++ src/app/shared/constants/precompromiso-estatus.constants.ts
@@ -1,8 +1,12 @@
 export const ESTATUS_PRECOMPROMISO = {
   CAPTURADO: 1,
   REVISADO: 2,
-  COMPROMETIDO: 3,
-  AUTORIZADO: 4,
+  AUTORIZADO: 3,
+  COMPROMETIDO: 4,
   RECHAZADO: 5,
-  CANCELADO: 6
+  CANCELADO: 6,
+  ELIMINADO: 7
 } as const;
+
+// Opcional: Si necesitas extraer solo los valores numéricos como un tipo
+export type TipoEstatusPrecompromiso = typeof ESTATUS_PRECOMPROMISO[keyof typeof ESTATUS_PRECOMPROMISO];
